309ea74c535e4905436d238e63971c96fde6c6b5
ypm <yesandno@secbit.io>
2022-12-05 11:08:47 +0800

    feat: substring new methord, return 32bits vec, more flexible(cal sha256)

--- crates/plookup-sha256/src/composer/substring.rs
+++ crates/plookup-sha256/src/composer/substring.rs
@@ -4,6 +4,306 @@ use ark_std::{format, vec::Vec};
 
 impl<F: Field> Composer<F> {
     /// "b" is substring of "a". should be padding to the max len.
+    /// l is the start index of "b" in "a".
+    /// m is the actual length of "b".
+    /// max_lens, b_max_lens must be a multiple of 32.
+    /// return Words(32bits) vec of "a_bits_location", "b_bits_location".
+    /// If create multiple private-substring in the circuit, make sure 'mask's are different.
+    pub fn add_substring_mask_poly_return_words(
+        &mut self,
+        a: &Vec<Variable>,
+        b: &Vec<Variable>,
+        mask: Variable,
+        l: Variable,
+        m: Variable,
+        max_lens: usize,
+        b_max_lens: usize,
+    ) -> Result<(Vec<Variable>, Vec<Variable>), Error> {
+        assert_eq!(max_lens % 32, 0);
+        assert_eq!(b_max_lens % 32, 0);
+        assert!(self.program_width >= 5);
+        if !self.switches.enable_private_substring {
+            self.switches.enable_private_substring = true;
+        }
+        if !self.selectors.contains_key("q_substring_r") {
+            let current_index = self.size();
+            self.selectors
+                .insert("q_substring_r".to_string(), vec![F::zero(); current_index]);
+        }
+        if !self.selectors.contains_key("q_substring") {
+            let current_index = self.size();
+            self.selectors
+                .insert("q_substring".to_string(), vec![F::zero(); current_index]);
+        }
+        assert!(!self.is_finalized);
+        
+        assert_eq!(a.len(), max_lens);
+        assert!(b.len() >= b_max_lens);
+        let mut b = b.clone()[0..b_max_lens].to_vec();
+        
+        let l_value = self.get_assignment(l);
+        let l_uint: Vec<u64> = l_value.into_repr().as_ref().into();
+        assert_eq!(l_uint[1], 0);
+        assert_eq!(l_uint[2], 0);
+        assert_eq!(l_uint[3], 0);
+        let l_uint = l_uint[0];
+        let m_value = self.get_assignment(m);
+        let m_uint: Vec<u64> = m_value.into_repr().as_ref().into();
+        assert_eq!(m_uint[1], 0);
+        assert_eq!(m_uint[2], 0);
+        assert_eq!(m_uint[3], 0);
+        let m_uint = m_uint[0];
+
+        let r_value = self.get_assignment(mask);
+
+        let mut comb = F::one();
+        let mut mask_poly_b = vec![];
+        let mut bit_location_b = vec![];
+
+        let mut mask_poly_a = vec![];
+        let mut bit_location_a = vec![];
+        for _ in 0..l_uint {
+            mask_poly_a.push(self.alloc(F::zero()));
+            bit_location_a.push(self.alloc(F::zero()))
+        }
+
+        for _ in 0..m_uint {
+            mask_poly_b.push(self.alloc(comb));
+            mask_poly_a.push(self.alloc(comb));
+            comb *= r_value;
+            bit_location_b.push(self.alloc(F::one()));
+            bit_location_a.push(self.alloc(F::one()));
+        }
+
+        for _ in m_uint..b_max_lens as u64 {
+            mask_poly_b.push(self.alloc(F::zero()));
+            bit_location_b.push(self.alloc(F::zero()));
+        }
+
+        let tmplen = mask_poly_a.len();
+        for _ in tmplen..max_lens {
+            mask_poly_a.push(self.alloc(F::zero()));
+            bit_location_a.push(self.alloc(F::zero()));
+        }
+        // padding b
+        for _ in b_max_lens..max_lens {
+            mask_poly_b.push(Composer::<F>::null());
+            bit_location_b.push(Composer::<F>::null());
+            b.push(Composer::<F>::null());
+        }
+
+        // prove ra and rb.
+        // w0:bit_location. w1:bit_location. w2:mask_poly. w3: mask_r
+
+        // must have this line at first
+        let index = self.insert_gate(vec![
+            Composer::<F>::null(),
+            Composer::<F>::null(),
+            Composer::<F>::null(),
+            mask,
+        ]);
+        self.selectors.get_mut(&format!("q_substring_r")).unwrap()[index] = F::one();
+
+        let mut current_index = self.size();
+        for i in 0..max_lens {
+            let wires = vec![
+                bit_location_a[i],
+                bit_location_a[i],
+                mask_poly_a[i],
+                mask,
+            ];
+
+            let index = self.insert_gate(wires);
+            assert_eq!(current_index, index);
+
+            self.selectors.get_mut(&format!("q_substring_r")).unwrap()[index] = F::one();
+            if i == max_lens - 1 {
+                self.selectors.get_mut(&format!("q_substring_r")).unwrap()[index] = F::zero();
+            }
+
+            // enforce_bool
+            self.selectors.get_mut("q_m").unwrap()[index] = F::one();
+            self.selectors.get_mut("q_0").unwrap()[index] = -F::one();
+
+            current_index += 1;
+        }
+
+        // must have this line at first
+        let index = self.insert_gate(vec![
+            Composer::<F>::null(),
+            Composer::<F>::null(),
+            Composer::<F>::null(),
+            mask,
+        ]);
+        self.selectors.get_mut(&format!("q_substring_r")).unwrap()[index] = F::one();
+
+        let mut current_index = self.size();
+        for i in 0..b_max_lens {
+            let wires = vec![
+                bit_location_b[i],
+                bit_location_b[i],
+                mask_poly_b[i],
+                mask,
+            ];
+
+            let index = self.insert_gate(wires);
+            assert_eq!(current_index, index);
+
+            self.selectors.get_mut(&format!("q_substring_r")).unwrap()[index] = F::one();
+            if i == b_max_lens - 1 {
+                self.selectors.get_mut(&format!("q_substring_r")).unwrap()[index] = F::zero();
+            }
+
+            // enforce_bool
+            self.selectors.get_mut("q_m").unwrap()[index] = F::one();
+            self.selectors.get_mut("q_0").unwrap()[index] = -F::one();
+
+            current_index += 1;
+        }
+
+        // prove substring
+        // w0:mask_poly_a. w1:a. w2:mask_poly_b. w3:b.
+        let mut current_index = self.size();
+        for i in 0..max_lens {
+            let wires = vec![mask_poly_a[i], a[i], mask_poly_b[i], b[i]];
+
+            let index = self.insert_gate(wires);
+            assert_eq!(current_index, index);
+
+            self.selectors.get_mut(&format!("q_substring")).unwrap()[index] = F::one();
+
+            current_index += 1;
+        }
+
+        // output bit_location_a
+        let bit_location_a_value = self.get_assignments(&bit_location_a);
+        let mut output_words_a = vec![];
+        for i in 0..max_lens / 32 {
+            let mut tmp_vars = vec![];
+            for j in 0..8 {
+                let tmp = bit_location_a_value[32 * i + 4 * j] * F::from(8u64)
+                    + bit_location_a_value[32 * i + 4 * j + 1] * F::from(4u64)
+                    + bit_location_a_value[32 * i + 4 * j + 2] * F::from(2u64)
+                    + bit_location_a_value[32 * i + 4 * j + 3];
+                tmp_vars.push(self.alloc(tmp));
+                self.poly_gate(
+                    vec![
+                        (tmp_vars[j], -F::one()),
+                        (bit_location_a[32 * i + 4 * j], F::from(8u64)),
+                        (bit_location_a[32 * i + 4 * j + 1], F::from(4u64)),
+                        (bit_location_a[32 * i + 4 * j + 2], F::from(2u64)),
+                        (bit_location_a[32 * i + 4 * j + 3], F::one()),
+                    ],
+                    F::zero(),
+                    F::zero(),
+                );
+            }
+
+            let tmp_vars_value = self.get_assignments(&tmp_vars);
+            let mut tmp2_vars = vec![];
+            for j in 0..2 {
+                let tmp = tmp_vars_value[4 * j] * F::from(1u64 << 12)
+                    + tmp_vars_value[4 * j + 1] * F::from(1u64 << 8)
+                    + tmp_vars_value[4 * j + 2] * F::from(1u64 << 4)
+                    + tmp_vars_value[4 * j + 3];
+                tmp2_vars.push(self.alloc(tmp));
+                self.poly_gate(
+                    vec![
+                        (tmp2_vars[j], -F::one()),
+                        (tmp_vars[4 * j], F::from(1u64 << 12)),
+                        (tmp_vars[4 * j + 1], F::from(1u64 << 8)),
+                        (tmp_vars[4 * j + 2], F::from(1u64 << 4)),
+                        (tmp_vars[4 * j + 3], F::one()),
+                    ],
+                    F::zero(),
+                    F::zero(),
+                );
+            }
+            
+            // 2 16bits
+            let tmp2_vars_value = self.get_assignments(&tmp2_vars);
+            
+            let tmp = tmp2_vars_value[0] * F::from(1u64 << 16)
+                + tmp2_vars_value[1];
+            output_words_a.push(self.alloc(tmp));
+            self.poly_gate(
+                vec![
+                    (output_words_a[i], -F::one()),
+                    (tmp2_vars[0], F::from(1u128 << 16)),
+                    (tmp2_vars[1], F::one()),
+                ],
+                F::zero(),
+                F::zero(),
+            );
+        }
+
+        // output bit_location_b
+        let bit_location_b_value = self.get_assignments(&bit_location_b);
+        let mut output_words_b = vec![];
+        for i in 0..b_max_lens / 32 {
+            let mut tmp_vars = vec![];
+            for j in 0..8 {
+                let tmp = bit_location_b_value[32 * i + 4 * j] * F::from(8u64)
+                    + bit_location_b_value[32 * i + 4 * j + 1] * F::from(4u64)
+                    + bit_location_b_value[32 * i + 4 * j + 2] * F::from(2u64)
+                    + bit_location_b_value[32 * i + 4 * j + 3];
+                tmp_vars.push(self.alloc(tmp));
+                self.poly_gate(
+                    vec![
+                        (tmp_vars[j], -F::one()),
+                        (bit_location_b[32 * i + 4 * j], F::from(8u64)),
+                        (bit_location_b[32 * i + 4 * j + 1], F::from(4u64)),
+                        (bit_location_b[32 * i + 4 * j + 2], F::from(2u64)),
+                        (bit_location_b[32 * i + 4 * j + 3], F::one()),
+                    ],
+                    F::zero(),
+                    F::zero(),
+                );
+            }
+
+            let tmp_vars_value = self.get_assignments(&tmp_vars);
+            let mut tmp2_vars = vec![];
+            for j in 0..2 {
+                let tmp = tmp_vars_value[4 * j] * F::from(1u64 << 12)
+                    + tmp_vars_value[4 * j + 1] * F::from(1u64 << 8)
+                    + tmp_vars_value[4 * j + 2] * F::from(1u64 << 4)
+                    + tmp_vars_value[4 * j + 3];
+                tmp2_vars.push(self.alloc(tmp));
+                self.poly_gate(
+                    vec![
+                        (tmp2_vars[j], -F::one()),
+                        (tmp_vars[4 * j], F::from(1u64 << 12)),
+                        (tmp_vars[4 * j + 1], F::from(1u64 << 8)),
+                        (tmp_vars[4 * j + 2], F::from(1u64 << 4)),
+                        (tmp_vars[4 * j + 3], F::one()),
+                    ],
+                    F::zero(),
+                    F::zero(),
+                );
+            }
+            
+            // 2 16bits
+            let tmp2_vars_value = self.get_assignments(&tmp2_vars);
+            
+            let tmp = tmp2_vars_value[0] * F::from(1u64 << 16)
+                + tmp2_vars_value[1];
+            output_words_b.push(self.alloc(tmp));
+            self.poly_gate(
+                vec![
+                    (output_words_b[i], -F::one()),
+                    (tmp2_vars[0], F::from(1u128 << 16)),
+                    (tmp2_vars[1], F::one()),
+                ],
+                F::zero(),
+                F::zero(),
+            );
+        }
+
+        Ok((output_words_a, output_words_b))
+    }
+    
+    /// Deprecated.
+    /// "b" is substring of "a". should be padding to the max len.
     /// a_max_lens == 2048, b_max_lens == 192.
     /// l is the start index of "b" in "a".
     /// m is the actual length of "b"
@@ -420,6 +720,7 @@ impl<F: Field> Composer<F> {
         Ok((output_bits_a, output_bits_b))
     }
 
+    /// Deprecated.
     /// a.len() == 1024
     pub fn add_substring_mask_poly_1024(
         &mut self,
